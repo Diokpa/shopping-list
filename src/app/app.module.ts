6ae3cc85bbbc555e78fd12c271a904dcbbecaae6
Diokpa <uwagwu.joseph@gmail.com>
2022-06-18 05:44:44 -0400

    Added component data binding

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/Components/header/header.component';
+import { RecipesComponent } from './features/recipes/recipes.component';
+import { RecipeListComponent } from './features/recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './features/recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './features/recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './pages/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './pages/shopping-list/shopping-edit/shopping-edit.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/pages/shopping-list/shopping-list.component.ts
@@ -0,0 +1,23 @@
+import { Ingredient } from './../../shared/Models/ingredients.model';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+ingredients: Ingredient[] = [
+  new Ingredient("Lemon", 2),
+  new Ingredient("Cucumber", 3)
+];
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+
